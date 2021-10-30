fe7988a03d80a48c58cd6a07e10e0d04e066a3ab
Bruno Dias <dias.h.bruno@gmail.com>
2021-10-30 17:15:14 -0300

    [feature] instantiate service and its dependencies.

--- instantiator.js
+++ instantiator.js
@@ -1,18 +1,49 @@
-let services = new Map();
+const CONSTRUCTOR_R = /\bconstructor\((.*)\)/;
+
+const deps = ds => ds.replace(/[\,\s]+/g, ',').split(',');
 
 module.exports = class Instantiator {
+  services = null;
+
+  constructor() {
+    this.services = new Map();
+  }
+
   add(service) {
     const instances = [];
-    const configuration = {};
-    services[service] = null;
+    const configuration = {
+      deps: [],
+      instance: null
+    };
+    const ds = (CONSTRUCTOR_R.exec(
+      service.toString()
+    ) || [])[1] || '';
+
+    this.services[service.name] = {
+      definition: service,
+      deps: ds ? deps(ds) : [],
+      instance: null
+    };
+  }
+
+  instantiate(service) {
+    const configuration = this.services[service],
+          { definition, deps } = configuration;
+    const dependencies = deps.map(d => this.get(d));
+    const instance = new definition(...dependencies);
+    configuration.instance = instance;
+    return instance;
   }
 
   instance(service) {
-    return services[service];
+    const _ = this.services[service];
+    if (!_) {
+      throw Error(`Service '${service}' not found`);
+    }
+    return _ && _.instance ? _.instance : this.instantiate(service);
   }
 
   get(service) {
-    const instance = this.instance(service);
-    return instance || (services[service] = new service());
+    return this.instance(service);
   }
 }
--- t/index.js
+++ t/index.js
@@ -1,24 +1,48 @@
 const assert = require('assert');
 const Instantiator = require('../instantiator.js');
 
+class A {}
+class B {}
+class C {}
+class S {
+  a = null;
+  b = null;
+  c = null;
+  constructor(A, B, C) {
+    this.a = A;
+    this.b = B;
+    this.c = C;
+  }
+}
+
 describe('Instantiator', () => {
   let i;
 
   beforeEach(() => (i = new Instantiator()));
 
   it('should create a new instance.', () => {
-    class S {}
-
-    i.add(S);
-
-    assert.ok(i.get(S) instanceof S);
+    i.add(A);
+    assert.ok(i.get(A.name) instanceof A);
   });
 
   it('must return the same instance.', () => {
-    class S {}
+    i.add(A);
+    assert.ok(i.get(A.name) === i.get(A.name));
+  });
 
+  it('must resolve its dependencies.', () => {
+    i.add(A);
+    i.add(B);
+    i.add(C);
     i.add(S);
+    assert.ok(i.get(A.name) === i.get(S.name).a);
+  });
 
-    assert.ok(i.get(S) === i.get(S));
+  it('must throw if it cannot resolve dependency.', () => {
+    try {
+      i.add(S);
+    } catch (e) {
+      assert.ok(/\'A\'/.exec(e.message)[1]);
+    }
   });
 });
