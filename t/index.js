200dcf043c26bf6e0c1a8c747cbe5831462b418b
Bruno Dias <dias.h.bruno@gmail.com>
2021-10-31 00:53:04 -0300

    [chore] removed mocha dependency.

--- t/index.js
+++ t/index.js
@@ -1,99 +1,113 @@
 const assert = require('assert');
 const Instantiator = require('../instantiator.js');
 
-describe('Instantiator', () => {
-  let i;
-
-  beforeEach(() => (i = new Instantiator()));
-
-  it('should create a new instance.', () => {
-    class A {}
-    i.addSingleInstance(A);
-    assert.ok(i.get(A.name) instanceof A);
-  });
-
-  it('must return the same instance.', () => {
-    class A {}
-    i.addSingleInstance(A);
-    assert.ok(i.get(A.name) === i.get(A.name));
-  });
-
-  it('must resolve its dependencies.', () => {
-    class A {}
-    class B {}
-    class C {}
-    class S {
-      constructor(A, B, C) {
-        this.a = A;
-        this.b = B;
-        this.c = C;
-      }
+[
+  [
+    'should create a new instance.', (i) => {
+      class A {}
+      i.addSingleInstance(A);
+      assert.ok(i.get(A.name) instanceof A);
     }
-    i.addSingleInstance(A).
-      addSingleInstance(B).
-      addSingleInstance(C).
-      addSingleInstance(S);
-    assert.ok(i.get(A.name) === i.get(S.name).a);
-  });
-
-  it('must throw if it cannot resolve dependency.', () => {
-    try {
+  ],
+  [
+    'must return the same instance.', (i) => {
+      class A {}
+      i.addSingleInstance(A);
+      assert.ok(i.get(A.name) === i.get(A.name));
+    }
+  ],
+  [
+    'must resolve its dependencies.', (i) => {
+      class A {}
+      class B {}
+      class C {}
       class S {
-        constructor(A) {
+        constructor(A, B, C) {
           this.a = A;
+          this.b = B;
+          this.c = C;
         }
       }
-      i.addSingleInstance(S);
-    } catch (e) {
-      assert.ok(/\'A\'/.exec(e.message)[1]);
+      i.addSingleInstance(A).
+        addSingleInstance(B).
+        addSingleInstance(C).
+        addSingleInstance(S);
+      assert.ok(i.get(A.name) === i.get(S.name).a);
     }
-  });
-
-  it('must instantiate a service and dont cache.', () => {
-    class T {}
-    class V {
-      constructor(T) {
-        this.t = T;
+  ],
+  [
+    'must throw if it cannot resolve dependency.', (i) => {
+      try {
+        class S {
+          constructor(A) {
+            this.a = A;
+          }
+        }
+        i.addSingleInstance(S);
+      } catch (e) {
+        assert.ok(/\'A\'/.exec(e.message)[1]);
       }
     }
-    i.addNoCache(T).
-      addSingleInstance(V);
-    assert.ok(i.get(T.name) !== i.get(V.name).t);
-  });
-
-  it('must instantiate a new service with same dependency.', () => {
-    class T {}
-    class V {
-      constructor(T) {
-        this.t = T;
+  ],
+  [
+    'must instantiate a service and dont cache.', (i) => {
+      class T {}
+      class V {
+        constructor(T) {
+          this.t = T;
+        }
       }
+      i.addNoCache(T).
+        addSingleInstance(V);
+      assert.ok(i.get(T.name) !== i.get(V.name).t);
     }
-    i.addSingleInstance(T).
-      addNoCache(V);
-    const v = i.get(V.name);
-    assert.ok(i.get(T.name) === v.t);
-    assert.ok(i.get(V.name) !== v);
-  });
-
-  it('same class different strategies (through extension).', () => {
-    class T {}
-    class V extends T {}
-    i.addSingleInstance(T).
-      addNoCache(V);
-    const v = i.get(V.name);
-    const t = i.get(T.name);
-    assert.ok(v !== t);
-    assert.ok(t === i.get(T.name));
-    assert.ok(v !== i.get(V.name));
-  });
-
-  it('dispose instances of services.', () => {
-    class T {}
-    class V {}
-    i.addSingleInstance(T).
-      addNoCache(V);
-    const t = i.get(T.name);
-    i.dispose();
-    assert.ok(t !== i.get(T.name));
-  });
+  ],
+  [
+    'must instantiate a new service with same dependency.', (i) => {
+      class T {}
+      class V {
+        constructor(T) {
+          this.t = T;
+        }
+      }
+      i.addSingleInstance(T).
+        addNoCache(V);
+      const v = i.get(V.name);
+      assert.ok(i.get(T.name) === v.t);
+      assert.ok(i.get(V.name) !== v);
+    }
+  ],
+  [
+    'same class different strategies (through extension).', (i) => {
+      class T {}
+      class V extends T {}
+      i.addSingleInstance(T).
+        addNoCache(V);
+      const v = i.get(V.name);
+      const t = i.get(T.name);
+      assert.ok(v !== t);
+      assert.ok(t === i.get(T.name));
+      assert.ok(v !== i.get(V.name));
+    }
+  ],
+  [
+    'dispose instances of services.', (i) => {
+      class T {}
+      class V {}
+      i.addSingleInstance(T).
+        addNoCache(V);
+      const t = i.get(T.name);
+      i.dispose();
+      assert.ok(t !== i.get(T.name));
+    }
+  ]
+].forEach(([title, test]) => {
+  try {
+    console.log(title);
+    test(new Instantiator());
+  } catch(e) {
+    console.log("[failed]", title);
+    console.log(e);
+    process.exit(1);
+  }
 });
